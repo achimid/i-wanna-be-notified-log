8c812f888d75ab094405280b876bedf2b269bb98
Lourran Olimpio Zorzi <achimid@hotmail.com>
2022-03-10 15:10:34 -0300

    Criando tela de acompanhamento de execução

--- src/log/log-service.js
+++ src/log/log-service.js
@@ -1,6 +1,7 @@
 const logger = require('../utils/logger')
 const logModel = require('./log-model')
 const { clearObj, dateBetween } = require('../utils/commons')
+const { getSocket } = require('../utils/socket-util') 
 
 const limit = parseInt(process.env.DEFAULT_ENDPOINT_LIST_LIMIT)
 
@@ -24,9 +25,14 @@ const findById = (id) => logModel.findById(id).lean()
 
 const removeByUuid = (uuid) => logModel.deleteMany({ uuid }).lean()
 
-const onMessage = (data) => insert(data).then(logger.info)
+const onMessage = (data) => insert(data).then(emitLog).then(logger.info)
 
-const insert = (data) => logModel.create(data)
+const insert = (data) => logModel.create(data).then(() => data)
+
+const emitLog = (data) => {
+    getSocket().emit(`i-wanna-be-notified-event-log`, data)
+    return data
+}
 
 module.exports = {
     insert,
---
+++ src/utils/socket-util.js
@@ -0,0 +1,27 @@
+const http = require('http')
+// const createSocket = require('socket.io')
+
+let socket = null
+
+const socketInit = (app) => {
+
+    const server = http.createServer(app)
+    socket = require('socket.io')(server, { cors:{origin: '*'}})    
+
+    console.info('Iniciando sockets...')
+    
+    socket.on('connection', (client) => {
+        console.log('Client conectado...', client.id)        
+        client.on('disconnect', () => console.log('Client desconectado...'))
+    })
+
+    return server
+}
+
+const getSocket = () => { return socket }
+
+
+module.exports = {
+    getSocket,
+    socketInit
+}
